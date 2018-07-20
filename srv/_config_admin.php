fc46362e7a9a17c396c97685e37a4738e63102ae
Steve Van Essche <steve.vanessche@gmail.com>
2018-07-20 09:51:14 +0200

    Try another config

--- srv/_config_admin.php
+++ srv/_config_admin.php
@@ -1,36 +1,5 @@
 <?php
-
-  /* uncomment FOR DEBUG */
-  error_reporting(E_ALL);
-  ini_set('display_errors', 1);
-
-  // Constants for database connexion
-  define('DB_HOST', 'localhost'); // Database Hostname
-  define('DB_NAME', 'dorothy_teaser'); // Database Name
-  define('DB_USER', 'root'); // Database Username
-  define('DB_PASS', 'c]u!+fHh[F\x>8U+'); // Database Password
-
-  // we initialize a new connexion
-  $db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASS);
-
-  // we start a session
-  session_start();
-
-  // General constants
-  define('HOME_URL','https://dorothycares.ovh/');
-  // define('SCRIPT_DIR','/home/tcbmediavc/dorothycares.io');
-  define('SCRIPT_DIR','..');
-  define('VERSION','1.0.1 Beta');
-
-  // we include the GoogeAPI files
-  require_once SCRIPT_DIR.'/GoogleAPI/vendor/autoload.php';
-
-  // insert class files
-	require_once SCRIPT_DIR.'/class/class.user.php';
-  require_once SCRIPT_DIR.'/class/class.implantation.php';
-  require_once SCRIPT_DIR.'/class/class.startup.php';
-  require_once SCRIPT_DIR.'/class/class.system.php';
-  require_once SCRIPT_DIR.'/class/class.form.php';
+  require_once('/srv/_config.php');
 
   /*  Function: securityCheck
 
